094276609fadc1eea66eca4a42c08cf90f5edfe1
crazycoder.io <mesut@artelax.com>
2023-10-27 00:17:18 +0300

    create new room router to handle room logic

---
+++ routes/room.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const RoomModel = require("../models/Room");
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const rooms = await RoomModel.find();
+  
+        return res.json(rooms);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { participants, communityAvatar, communityName } = req.body;
+
+        const roomId = Math.random().toString(16).slice(8);
+        const chatRoomAuthId = Math.random().toString(16).slice(4); // TODO: Will be update with NodeJS SDK
+
+        const isRoomExist = await RoomModel.findOne({participants});
+
+        if (isRoomExist) return res.json(isRoomExist);
+
+        const newRoom = await RoomModel.create({
+            roomId,
+            chatRoomAuthId,
+            participants,
+            ...(communityAvatar && communityName && { communityAvatar, communityName})
+        });
+
+        // TODO: Create room instance on Commt with NodeJS SDK
+  
+        return res.json(newRoom);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+  
+module.exports = router;
