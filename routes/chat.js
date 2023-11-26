3d02711f94a66deb73ffa497c3999d871a3148ff
crazycoder.io <mesut@artelax.com>
2023-11-26 17:36:56 +0300

    add console log for `/load-more-messages` to follow request

--- routes/chat.js
+++ routes/chat.js
@@ -25,15 +25,20 @@ router.get('/load-more-messages', async (req, res) => {
     try {
         const {roomId, offset, limit} = req.query;
 
+        console.log('------ req.query -->>', req.query);
+
         // Offset field represents "page" and help us to skip certain amount of records
         const skip = offset * limit;
 
         const totalRecords = await MessageModel.countDocuments({roomId});
         const messages = await MessageModel.find({roomId}).skip(skip).limit(limit);
 
+        console.log('------ totalRecords -->>', totalRecords);
+        console.log('------ messages -->>', messages);
+
         const hasNext = totalRecords - skip > limit; // If there is no more records with this skip and limit properties, set hasNext to false else set hasNext to true
 
-        res.status(200).json({messages, hasNext});
+        res.json({messages, hasNext});
     } catch (err) {
         res.status(500).send(err.message);
     }
