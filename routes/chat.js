6dc78e192afd89997b92b5776db88c24f2e3490f
crazycoder.io <mesut@artelax.com>
2023-11-27 11:55:58 +0300

    update main chat endpoint to retrieve messages both for load more message and initial

--- routes/chat.js
+++ routes/chat.js
@@ -19,29 +19,24 @@ router.post('/save-message', async (req, res) => {
     }
 });
 
-router.get('/load-more-messages', async (req, res) => {
-    try {
-        const {roomId, offset, limit} = req.query;
-
-        // Offset field represents "page" and help us to skip certain amount of records
-        const skip = offset * limit;
-
-        const totalRecords = await MessageModel.countDocuments({roomId});
-        const messages = await MessageModel.find({roomId}).skip(skip).limit(limit);
-
-        const hasNext = totalRecords - skip > limit; // If there is no more records with this skip and limit properties, set hasNext to false else set hasNext to true
-
-        res.status(200).json({messages, hasNext});
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
 router.get('/', async (req, res) => {
     try {
-        const messages = await MessageModel.find();
+        const {roomId, offset, limit} = req.query;
+        let messages, hasNext;
+
+        if (roomId && offset && limit) {
+            // Offset field represents "page" and help us to skip certain amount of records
+            const skip = offset * limit;
+
+            const totalRecords = await MessageModel.countDocuments({roomId});
+            messages = await MessageModel.find({roomId}).skip(skip).limit(limit);
+
+            hasNext = totalRecords - skip > limit; // If there is no more records with this skip and limit properties, set hasNext to false else set hasNext to true
+        } else {
+            messages = await MessageModel.find().sort({createdAt: -1}).limit(limit || 10);
+        }
   
-        return res.json(messages);
+        return res.json({messages, hasNext});
     } catch (err) {
         res.status(500).send(err.message);
     }
